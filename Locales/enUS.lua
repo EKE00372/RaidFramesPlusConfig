dc2b42030987c6ae88dce130a6dfe0c41a673f44
merywow1 <136429825+merywow1@users.noreply.github.com>
2023-06-15 08:34:50 +0200

    Update enUS.lua

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,1 +1,13 @@
+local e=select(2,...)
+if e.locale ~= "enUS" and e.locale ~= "enGB" then return end
 
+e[1]="RaidFramesPlus  Config  Panel"
+e[2]="Player position"
+e[3]="Double sort raid 'combined groups' and party frames" -- 'combined groups' must be equal to the string in EditMode > Raid frames > Groups option
+e[4]=" Place pets under owner"
+e[5]="Buff and debuff sizes"
+e[6]=" Hide roles"
+e[7]=" Hide names"
+e[8]="Macro |cff00F0F0RaidFramesPlus|r created."   -- print()
+e[9]="Macro |cff00F0F0RaidFramesPlus|r already exists." -- print()
+e[10]="Too much macros ?"  -- print()
