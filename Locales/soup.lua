2f58241d3642fd8c1aa88f1867aef10c5efff393
EKE00372 <eke00372@gmail.com>
2023-06-16 06:31:39 +0800

    revert soup.lua

--- Locales/soup.lua
+++ Locales/soup.lua
@@ -1,17 +1,18 @@
 local e=select(2,...)
 if e[1] then return end
 
-e[1]="RaidFramesPlus  Config  Panel"
-e[2]="Player position"
-e[3]="Double sort raid" -- 'combined groups' must be equal to the string in EditMode > Raid frames > Groups option
-e[4]=" Place pets under owner"
-e[5]="Buff and debuff sizes"
-e[6]=" Hide roles"
-e[7]=" Hide names"
-e[8]="Macro |cff00F0F0RaidFramesPlus|r created."   -- print()
-e[9]="Macro |cff00F0F0RaidFramesPlus|r already exists." -- print()
-e[10]="Too much macros ?"  -- print()
+e[1]="RaidFramesPlus  Kontrol  Panelo"
+e[2]="Player pozicio"
+e[3]="Double sort raid 'combined groups' and party frames"
+e[4]=" Place "..PETS:lower().." under owner"
+e[5]=AURAS.." : "..HUD_EDIT_MODE_SETTING_AURA_FRAME_ICON_SIZE:lower()
+e[6]=" "..HIDE.." : "..HUD_EDIT_MODE_SETTING_UNIT_FRAME_SORT_BY_SETTING_ROLE:lower()
+e[7]=" "..HIDE.." : "..NAME:lower()
+e[8]=MACRO.." |cff00F0F0RaidFramesPlus|r created."
+e[9]=MACRO.." |cff00F0F0RaidFramesPlus|r already exists."
+e[10]=MACRO.." bank full ?"
 e[11]="Ignore sort"
 e[12]="Set to end"
 e[13]="Both sort can only works on 'combined groups' raid and party frames"
 e[14]="Create Macro"
+
